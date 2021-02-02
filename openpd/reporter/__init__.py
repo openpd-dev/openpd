1b04eccdb6358561f23ccf497e4cd935a32ae0bc
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-02-03 00:57:37 +0800

    Add SequenceLoader and PDBLoader

--- openpd/reporter/__init__.py
+++ openpd/reporter/__init__.py
@@ -3,3 +3,7 @@ __maintainer__ = "Zhenyu Wei"
 __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "MIT"
+
+from .pdbReporter import PDBReporter
+
+__all__ = ['PDBReporter']
---
+++ openpd/reporter/pdbreporter.py
@@ -0,0 +1,3 @@
+class PDBReporter(object):
+    def __init__(self) -> None:
+        super().__init__()
