1dce6b9a02c7f9470e6fa2fea8efb5ae4fd3f39f
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-02-20 18:18:01 +0800

    Add getNormVec to utils and fix bug for getAngle and getTorsion for Quantity

--- openpd/utils/__init__.py
+++ openpd/utils/__init__.py
@@ -4,14 +4,14 @@ __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "GPLv3"
 
-from .geometry import getBond, getAngle, getTorsion
 from .judgement import isAlmostEqual, isArrayEqual, isArrayAlmostEqual, isArrayLambda
+from .geometry import getBond, getNormVec, getAngle, getTorsion
 from .unique import uniqueList, mergeSameNeighbor
 from .locate import findFirst, findFirstLambda, findAll, findAllLambda, binarySearch
 
 __all__ = [
-    'getBond', 'getAngle', 'getTorsion',
     'isAlmostEqual', 'isArrayEqual', 'isArrayAlmostEqual', 'isArrayLambda',
+    'getBond', 'getNormVec', 'getAngle', 'getTorsion',
     'uniqueList', 'mergeSameNeighbor',
     'findFirst', 'findFirstLambda', 'findAll', 'findAllLambda', 'binarySearch'
 ]
