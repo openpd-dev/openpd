8c442af8a0c3d8e13bf2e412eb2f5647a0d88f31
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-02-24 22:45:11 +0800

    Update PDFFTorsionForce

--- openpd/tests/test_pdffTorsionForceField.py
+++ openpd/tests/test_pdffTorsionForceField.py
@@ -2,12 +2,13 @@ import pytest, os
 import numpy as np
 from numpy import pi
 from .. import PDFFTorsionForceField
-from .. import findFirstLambda, findAll, findAllLambda, isAlmostEqual, isArrayEqual
+from .. import isArrayEqual
+from ..unit import *
 
 cur_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 force_field_dir = os.path.join(cur_dir, '../data/pdff/torsion')
 
-class TestPDFFTorsionForce:
+class TestPDFFTorsionForceField:
     def setup(self):
         self.force_field = PDFFTorsionForceField('ASN', 'LEU')
 
@@ -16,14 +17,30 @@ class TestPDFFTorsionForce:
 
     def test_attributes(self):
         assert self.force_field.name == 'ASN-LEU'
-        assert isArrayEqual(self.force_field.target_coord, np.linspace(-pi, pi, 600))
+        assert isArrayEqual(self.force_field._target_coord, np.arange(-pi, pi+0.001, 0.001))
+        assert self.force_field.derivative_width == 0.0001
 
     def test_exceptions(self):
         with pytest.raises(AttributeError):
             self.force_field.name = 1
-        
+            
         with pytest.raises(AttributeError):
-            self.force_field.target_coord = 1
+            self.force_field.derivative_width = 1
 
-        with pytest.raises(AttributeError):
-            self.force_field.target_data = 1
+    def test_getEnergy(self):
+        asn_leu_force_field = np.load(os.path.join(force_field_dir, 'ASN-LEU.npy'))
+        coord = np.load(os.path.join(force_field_dir, 'coord.npy'))
+        assert (
+            self.force_field.getEnergy(coord[2]) / kilojoule_permol ==
+            pytest.approx(asn_leu_force_field[2])
+        )
+        
+    def test_getForce(self):
+        coord = np.load(os.path.join(force_field_dir, 'coord.npy'))
+        assert (
+            self.force_field.getForce(coord[2]) == 
+            - (
+                self.force_field.getEnergy(coord[2]+0.5*self.force_field.derivative_width) - 
+                self.force_field.getEnergy(coord[2]-0.5*self.force_field.derivative_width)
+            ) / self.force_field.derivative_width / angstrom
+        )
