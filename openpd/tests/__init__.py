6adfe787aa1caf881c6aeff28fe4c6a477d0cdf1
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-03-01 10:28:26 +0800

    Add comment to __init__.py of openpd.tests

--- openpd/tests/__init__.py
+++ openpd/tests/__init__.py
@@ -3,3 +3,5 @@ __maintainer__ = "Zhenyu Wei"
 __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "GPLv3"
+
+# This file exist for the relative import of test file
