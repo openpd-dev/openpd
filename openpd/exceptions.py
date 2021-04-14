95c9fffb620f43715811db45f004ccc8033b3325
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-04-14 17:36:10 +0800

    Rename EditBoundAttributeError to ModifiedBoundMoleculeError

--- openpd/exceptions.py
+++ openpd/exceptions.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+file: exceptions.py
+created time : 2021/03/11
+last edit time : 2021/04/14
+author : Zhenyu Wei 
+version : 1.0
+contact : zhenyuwei99@gmail.com
+copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+'''
+
 class NonboundError(Exception):
     """
-    NonboundError related to the binding action contained in openpd.force, openpd.integrator, and openpd.dumper package
+    NonboundError related to the binding action 
+    Used in:
+    - openpd.force
+    - openpd.integrator 
+    - openpd.dumper
+    - openpd.element.atom
+    - openpd.element.molecule
     """    
     pass
 
@@ -10,9 +28,21 @@ class RebindError(Exception):
     """ 
     pass
 
+class ModifiedBoundMoleculeError(Exception):
+    """
+    ModifiedBoundMoleculeError raises when edited the attribute of a bound instance
+    Used in:
+    - openpd.element.atom
+    - openpd.element.molecule
+    """
+
 class PeptideTypeError(Exception):
     """
-    PeptideTypeError related to the peptide type error contained in openpd.element.peptide module, openpd.loader, and openpd.force package
+    PeptideTypeError related to the peptide type error
+    Used in
+    - openpd.element.peptide 
+    - openpd.loader 
+    - openpd.force package
     """ 
     pass
 
