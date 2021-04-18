7395d56fcd0da247dea3f792308064296acfbdb4
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-04-18 21:16:35 +0800

    Update badage and add header to test file

--- openpd/runTest.py
+++ openpd/runTest.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+file: runTest.py
+created time : 2021/02/04
+last edit time : 2021/04/18
+author : Zhenyu Wei 
+version : 1.0
+contact : zhenyuwei99@gmail.com
+copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+'''
+
 import pytest, os, argparse
 cur_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 test_dir = os.path.join(cur_dir, 'tests')
